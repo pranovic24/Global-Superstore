7cc7ee0ad0a0da4f62db80a7eddecc56d79588d9
Pranay Sood <pranay.sd@gmail.com>
2023-01-18 00:32:31 +0530

    finished implementing update functionality

--- src/main/java/com/ltp/globalsuperstore/Item.java
+++ src/main/java/com/ltp/globalsuperstore/Item.java
@@ -1,6 +1,7 @@
 package com.ltp.globalsuperstore;
 
 import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -11,16 +12,18 @@ public class Item {
     private Double discount;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
+    private String id;
 
-    public Item(String category, String name, Double price, Double discount, Date date) {
-        this.category = category;
-        this.name = name;
-        this.price = price;
-        this.discount = discount;
-        this.date = date;
-    }
+    // public Item(String category, String name, Double price, Double discount, Date date) {
+    //     this.category = category;
+    //     this.name = name;
+    //     this.price = price;
+    //     this.discount = discount;
+    //     this.date = date;
+    // }
         
     public Item() {
+        this.id = UUID.randomUUID().toString();
     }
 
     public String getCategory() {
@@ -63,8 +66,12 @@ public class Item {
         this.date = date;
     }
 
+    public String getId() {
+        return this.id;
+    }
 
-
-
+    public void setId(String id) {
+        this.id = id;
+    }
 
 }
--- src/main/java/com/ltp/globalsuperstore/SuperstoreController.java
+++ src/main/java/com/ltp/globalsuperstore/SuperstoreController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SuperstoreController {
@@ -14,15 +15,24 @@ public class SuperstoreController {
     private List<Item> items = new ArrayList<Item>();
 
     @GetMapping("/")
-    public String getForm(Model model) {
-        model.addAttribute("item", new Item());
+    public String getForm(Model model, @RequestParam(required = false) String id) {
+        int index = getIndexFromId(id);
+        model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : items.get(index));
         model.addAttribute("categories", Constants.CATEGORIES);
+        // System.out.println("id: " + id);
         return "form";
     }
 
     @PostMapping("/submitItem") 
     public String handleSubmit(Item item) {
-        items.add(item);
+        // System.out.println(item.getId());
+        int index = getIndexFromId(item.getId());
+        if (index == Constants.NOT_FOUND) {
+            items.add(item);
+        } else {
+            items.set(index, item);
+        }
+        // items.add(item);
         return "redirect:/inventory";
     }
     
@@ -30,6 +40,15 @@ public class SuperstoreController {
     public String getInventory(Model model) {
         model.addAttribute("items", items);
         return "inventory";
+    }
+
+    public int getIndexFromId(String id) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId().equals(id)) {
+                return i;
+            }
+        }
+        return Constants.NOT_FOUND;
     }
 
 }
