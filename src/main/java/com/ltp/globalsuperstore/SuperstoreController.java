4bf9885fea77fc93b003c6478c5bc0619cdc380d
Pranay Sood <pranay.sd@gmail.com>
2023-01-17 23:17:28 +0530

    created Item class and binded item object to form

--- src/main/java/com/ltp/globalsuperstore/SuperstoreController.java
+++ src/main/java/com/ltp/globalsuperstore/SuperstoreController.java
@@ -1,20 +1,34 @@
 package com.ltp.globalsuperstore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SuperstoreController {
 
+    private List<Item> items = new ArrayList<Item>();
+
     @GetMapping("/")
     public String getForm(Model model) {
+        model.addAttribute("item", new Item());
         model.addAttribute("categories", Constants.CATEGORIES);
         return "form";
     }
 
+    @PostMapping("/submitItem") 
+    public String handleSubmit(Item item) {
+        items.add(item);
+        return "redirect:/inventory";
+    }
+    
     @GetMapping("/inventory")
     public String getInventory() {
         return "inventory";
     }
+
 }
